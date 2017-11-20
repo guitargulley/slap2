3c68922430714024ea5aa90e39ff70aeb25cbeb3
Brandon Gulley <brandongulley@Brandons-MacBook-Pro.local>
2017-11-20 16:54:53 -0700

    add mods created

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="HelloWorld">
-
-    <div v-for="player in players" class="player" :class="getHealthStatus(player)">
-
-      <img :src="player.picture" alt="">
+    <div v-for="player in players" class="player col-xs-12 col-sm-6 col-md-4" :class="getHealthStatus(player)">
+      
+      <img :src="player.picture" height="250px" width="auto" alt="">
       <h1>Player: {{player.playerName}}</h1>
       <h4 v-if="player.health >= 1">Health: {{player.health}}</h4>
       <h4 v-else>DEAD</h4>
-
+      
+      <button v-for="item in items" @click="addItem(item, player)">{{item.name}}</button>
+      
+      
       <button v-for="(val, attack) in player.attacks" :disabled="player.health <= 1" @click="slap(player, attack)">{{attack}}</button>
-
     </div>
   </div>
 </template>
@@ -23,8 +24,11 @@
       }
     },
     methods: {
-      slap(player, attack) {
+      slap(player, attack, item) {
         this.$store.dispatch('slap', { player, attack })
+      },
+      addItem(item, player){
+        this.$store.dispatch('addItem', {item, player})
       },
       getHealthStatus(player) {
         return {
@@ -37,6 +41,9 @@
     computed: {
       players() {
         return this.$store.state.players
+      },
+      items() {
+        return this.$store.state.items
       }
     }
   }
--- src/store/index.js
+++ src/store/index.js
@@ -3,65 +3,112 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 
+
+function addMods(player) {
+    debugger
+    var total = 1;
+    for (var i = 0; i < player.items.length; i++) {
+        var item = player.items[i];
+        total += item.modifier
+    }
+    return total
+}
+
+
+
 export default new Vuex.Store({
     state: {
         // THIS IS THE LOCAL COPY OF ALL DATA FOR THE APPLICATION
         players: [
             {
-              picture: '//placehold.it/100x100',
-              health: 100,
-              playerName: 'Mark',
-              attacks: {
-                  slap: 1,
-                  kick: 5
-              }
+                picture: 'https://timedotcom.files.wordpress.com/2015/01/mark-wahlberg-marky-mark.jpg',
+                health: 100,
+                playerName: 'Mark',
+                attacks: {
+                    slap: 1,
+                    kick: 5
+                },
+                items: []
             },
             {
-              picture: '//placehold.it/100x100',
-              health: 100,
-              playerName: 'D$',
-              attacks: {
-                  punch: -5, 
-                  haduken: -1000
-              }
+                picture: 'https://static.giantbomb.com/uploads/scale_small/0/6865/229647-flava_flav.0.jpg',
+                health: 100,
+                playerName: 'D$',
+                attacks: {
+                    punch: -5,
+                    haduken: -1000
+                },
+                items: []
             },
             {
-              picture: '//placehold.it/100x100',
-              health: 100,
-              playerName: 'Jakob',
-              attacks: {
-                  tickle: -100
-              }
+                picture: 'http://thecount.com/wp-content/uploads/Jake-from-State-Farm-did-he-die-400x364.jpeg',
+                health: 100,
+                playerName: 'Jakob',
+                attacks: {
+                    tickle: -100
+                },
+                items: []
             },
             {
-              picture: '//placehold.it/100x100',
-              health: 10,
-              playerName: 'J-Dawg',
-              attacks: {
-                  flu: 10000
-              }
+                picture: 'http://media.tmz.com/2016/12/15/1215-pitbull-tmz-8.jpg',
+                health: 10,
+                playerName: 'J-Dawg',
+                attacks: {
+                    flu: 10000
+                },
+                items: []
             }
-          ]
+        ],
+        items: [
+
+            {
+                name: 'flame',
+                modifier: .8
+            },
+            {
+                name: 'spike',
+                modifier: .5
+            },
+            {
+                name: 'shield',
+                modifier: -.3
+            },
+
+        ]
+
+
     },
     mutations: {
         // FUNCTIONS THAT CAN CHANGE THE STATE
-        slap(state, payload){
+        slap(state, payload) {
             // DATA MUTATION
-            payload.player.health -= payload.player.attacks[payload.attack]
-        }
+            payload.player.health -= payload.player.attacks[payload.attack] * addMods(payload.player)
+            payload.player.items = []
+        },
+        addItem(state, payload) {
+            payload.player.items.push(payload.item)
+        },
+
     },
     actions: {
         // FUNCTIONS THAT REQUESTS TO CHANGE DATA
-        slap({ commit, dispatch }, payload){
+        slap({ commit, dispatch }, payload) {
             // BUSINESS LOGIC
-            if(payload.player.health > 0){
+            if (payload.player.health > 0) {
                 // ALLOW THE SLAP
                 commit('slap', payload)
-            }else{
+            } else {
                 // YOU MONSTER
                 console.log('nope')
             }
+        },
+        addItem({ commit, dispatch }, payload) {
+            if (payload.player.items.indexOf(payload.item) == -1) {
+                commit('addItem', payload)
+            }
+
         }
+
     }
-    
+
 })
